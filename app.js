acb942b422890ac8b77f884cd7c5e3fc731d0b20
qutub ahmed <qutubahmed104@gmail.com>
2025-01-25 12:31:22 +0500

    do some changes in navbar

--- app.js
+++ app.js
@@ -14,20 +14,33 @@
 
 let random = document.querySelector(".menu-btn");
 
-random.addEventListener("click", ()=>{
+random.addEventListener("click", () => {
     const nav = document.querySelector("#second-nav");
-    nav.style.left = "00px";
+    nav.style.left = "0px";
     nav.style.display = 'flex';
+    document.body.style.overflow = "hidden";
 })
 let closee = document.querySelector(".close");
 
-closee.addEventListener("click", ()=>{
+closee.addEventListener("click", () => {
     const nav = document.querySelector("#second-nav");
     nav.style.left = "-560px"
 })
 
 let input = document.querySelector("input");
 
-input.addEventListener("focusout", (e)=>{
-    e.target.value = '';;
+input.addEventListener("focusout", (e) => {
+    e.target.value = '';
 })
+
+const imageDiv = document.querySelector('.image');
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+    card.addEventListener('mouseenter', () => {
+        const bgImage = card.getAttribute('data-bg'); // Get the data attribute value
+        imageDiv.style.backgroundImage = `url(${bgImage})`;
+        
+    });
+});
+
