20cbb7dc4022156241d09c5c76419b1eae43590b
qutub ahmed <qutubahmed104@gmail.com>
2025-01-25 12:58:53 +0500

    did troubleshoot in javascript code

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ let closee = document.querySelector(".close");
 closee.addEventListener("click", () => {
     const nav = document.querySelector("#second-nav");
     nav.style.left = "-560px";
-    document.body.style.overflow = "auto";
+    document.body.style.overflow = "visible";
 })
 
 let input = document.querySelector("input");
